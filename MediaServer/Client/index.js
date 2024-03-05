da5ff987afc86888829e160889a41649a5fbf967
madoleeee <madol.eeee@gmail.com>
2024-03-05 22:52:01 +0900

    feature:테일윈드 및 기본 라우트 추가

--- MediaServer/Client/index.js
+++ MediaServer/Client/index.js
@@ -1,9 +1,10 @@
+/// <reference types="nativewind/types" />
 /**
  * @format
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import {Route} from "./src/app/route/v1/route";
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Route);
